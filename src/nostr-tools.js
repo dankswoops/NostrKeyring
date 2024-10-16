fe5070f688bcb8e833537d559ddcb102046cfc88
Chris Daley <chebizarro@gmail.com>
2024-10-16 09:13:12 -0700

    fix signing

--- src/nostr-tools.js
+++ src/nostr-tools.js
@@ -1,2 +1,4 @@
-import * as nostrTools from 'nostr-tools'
-export default nostrTools
+import { finalizeEvent, verifyEvent } from 'nostr-tools/pure';
+import { nip04, nip44, nip19 } from 'nostr-tools';
+
+export { finalizeEvent, verifyEvent, nip04, nip44, nip19 };
